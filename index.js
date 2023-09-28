bfa27259464e43603cf9dd63f927d36802ace8bd
jaspalsaini <sainijaspal5@gmail.com>
2023-09-29 00:47:00 +0530

    netlify install

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-import { Configuration, OpenAIApi } from 'openai'
-import { process } from './env'
-
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-})
-
-const openai = new OpenAIApi(configuration)
-
 const chatbotConversation = document.getElementById('chatbot-conversation')
  
 let conversationStr = ''
